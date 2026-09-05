de36ff2d5be703974a5eadf4c3598de297862ceb
Pranav Pawar <pranavpawar1001@gmail.com>
2026-09-05 15:12:28 +0530

    Chnage is frontend

--- frontend/vite.preview.config.js
+++ frontend/vite.preview.config.js
@@ -4,7 +4,7 @@ export default {
   preview: {
     proxy: {
       '/api': {
-        target: 'http://backend:8085',
+        target: 'http://backend:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
