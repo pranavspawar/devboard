4b9583a342b01810ae37f75d80d532fc3d28be63
Ubuntu <ubuntu@ip-172-31-43-219.us-west-2.compute.internal>
2026-09-23 17:50:55 +0000

    Chnages in file

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -33,7 +33,7 @@ export function DashboardPage() {
               workspace overview
             </span>
           </div>
-          <h1 className="text-[30px] font-semibold leading-none tracking-[-0.6px]">Welcome-back To EY and deloit</h1>
+          <h1 className="text-[30px] font-semibold leading-none tracking-[-0.6px]">Welcome-back To EY</h1>
           <p className="text-[14px] text-ink-600 dark:text-ink-400 mt-2">
             Here's where your work stands today.
           </p>
